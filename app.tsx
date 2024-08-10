c02d7c5c79fbd217a329e7d0052ff1e3ba4df053
Ryan Taylor <ryanewtaylor@gmail.com>
2024-08-10 14:19:32 -0400

    chore: show non-tree shaking version

--- app.tsx
+++ app.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // does not tree shake
-// import { faEnvelope, faGolfBall } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faGolfBall } from "@fortawesome/free-solid-svg-icons";
 
 // tree shakes
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope";
-import { faGolfBall } from "@fortawesome/free-solid-svg-icons/faGolfBall";
+// import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope";
+// import { faGolfBall } from "@fortawesome/free-solid-svg-icons/faGolfBall";
 
 export function App() {
   return (
